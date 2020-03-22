c795300dbb2e1836062a94ac9c360e4f7360e815
bertbarn <bob.ebarnhart@gmail.com>
2020-03-21 22:44:56 -0500

    added in conversion, made into a class instance, added argparsing

--- tts.py
+++ tts.py
@@ -3,49 +3,86 @@ import PyPDF2
 import logging
 import argparse
 
-def file_to_pdf(filename):
-    logger = logging.getLogger("TTS")
-    try:
-        assert isinstance(filename,str)
-        file = open(filename,'rb')
-        pdf_file = PyPDF2.PdfFileReader(file)
 
-        return pdf_file
-    except AssertionError:
-        logger.error("filename is not a string!")
-        exit(1)
-    except FileExistsError:
-        logger.error("file does not exist")
-        exit(1)
-    except FileNotFoundError:
-        logger.error("file not found")
-        exit(1)
+class ArxivTTS:
+    '''
+        Initial class to handle the conversion from pdf to text
+    '''
 
-def run(filename):
-    logger = logging.getLogger("TTS")
-    pdf = file_to_pdf(filename)
+    __name__ = "arxivTTS"
 
-    if pdf is None:
-        logger.error("file_to_pdf returned an error, exiting")
-        exit(1)
+    def __init__(self):
 
-    print(pdf.pages)
+        self.logger = self.__setup_logging__()
+        self.logger.info("welcome to arxiv text to speech conversion!")
+
+    def __file_to_pdf__(self,filename):
+        '''
+        private
+        :param filename: string of a pdf file
+        :return: pdf class of the file
+        '''
+
+        try:
+            assert isinstance(filename, str), "not a string"
+            file = open(filename, 'rb')
+            pdf_file = PyPDF2.PdfFileReader(file)
+
+            return pdf_file
+        except AssertionError as e:
+            self.logger.error(e.args[0])
+            exit(1)
+        except FileExistsError:
+            self.logger.error("file does not exist")
+            exit(1)
+        except FileNotFoundError:
+            self.logger.error("file not found")
+            exit(1)
+
+    def __setup_logging__(self):
+        '''
+        initializes the logger for printing
+        :return: nothing
+        '''
+
+        logger = logging.getLogger("arxivTTS")
+        stdout = logging.StreamHandler()
+        formatter = logging.Formatter('%(name)s::%(message)s')
+        stdout.setFormatter(formatter)
+        logger.addHandler(stdout)
+        logger.setLevel("DEBUG")
+
+        return logger
+
+    def run(self,filename):
+        '''
+        public, runs the conversion to mp3
+        :param filename: string of path leading to filename
+        :return: nothing
+        '''
+
+        pdf = self.__file_to_pdf__(filename)
+        file_title = filename.split("/")[-1].split(".")[0]
+        txt = ""
+
+        for num,pages in enumerate(pdf.pages):
+            self.logger.info("pre-processing page {}".format(num))
+            txt += pages.extractText()
+
+        language = 'en'
+        ob = gTTS(text=txt, lang=language, slow=False)
+        ob.save(file_title+".mp3")
+
+        if pdf is None:
+            self.logger.error("file_to_pdf returned an error, exiting")
+            exit(1)
 
 
-'''
-txt = page.extractText()
-
-language = 'en'
-
-ob = gTTS(text=txt,lang=language,slow=False)
-
-ob.save("test.mp3")
-
-'''
 if __name__ == "__main__":
 
-    logger = logging.getLogger("TTS")
-    logger.setLevel("INFO")
-    logger.info("welcome to arxiv text to speech conversion!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',default=None)
+    args = parser.parse_args()
 
-    run("test")
+    arxiv_tts = ArxivTTS()
+    arxiv_tts.run(args.filename)
